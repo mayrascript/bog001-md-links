b58c700752a17af3376a2b454aacf728a830ad61
Mayra <mayrarodriguez1709@gmail.com>
2020-09-01 10:01:24 -0500

    feat: validate links

--- validation.js
+++ validation.js
@@ -1,12 +1,38 @@
+const fs = require('fs')
+const pathExt = require('path');
+const marked = require('marked');
+
+
 const init = (path, options = { validate: false}) => {
   return new Promise((resolve, reject) => {
+
     if(!path) {
       console.error('You must provide a file to find links')
       reject(new Error('You must provide a file to find links'));
     }
+    const isValidExt = pathExt.extname(path) === '.md';
 
-    return resolve(console.log('checking...'));
+    if(!isValidExt) {
+      reject(new Error('The file extension is invalid'));
+    }
 
+    if(!fs.existsSync(path)) {
+      reject(new Error('The file path is invalid'));
+    }
+
+    const markdown = fs.readFileSync(path).toString();
+
+    const links = [];
+
+    const renderer = new marked.Renderer();
+
+    renderer.link = function (href, title, text) {
+      links.push({href, text, file: path});
+    };
+
+    marked(markdown, { renderer: renderer });
+
+    return resolve(links);
 
   });
 };
